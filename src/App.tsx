4263c467f1b253591e3d70a7d8407b3366284c5f
Jada Quandt <jadaquandt@gmail.com>
2022-09-27 09:58:20 -0400

    Building it out

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import Form from './Form';
 import Table from './Table';
 
 function App() {
+    const { title, content } = {
+        title: "Title One",
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.`
+      };
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Accessibility Examples</h1>
+                <p>Code This, Not That!</p>
+                <p>To use this website fully, we recommend using a screenreader like <a href="https://www.nvaccess.org/download/">NVDA</a></p>
             </header>
-            <Accordion />
+            <Accordion title={title} content={content} />
+            <Accordion title={title} content={content} />
             <Form />
             <Table />
             <h2>Example of an accessible image:</h2>
--- src/Form.tsx
+++ src/Form.tsx
@@ -4,6 +4,12 @@ function Form() {
   return (
     <div className="Form">
         <p>This is an example form component</p>
+        <form name="Registration Form">
+        <label htmlFor="first-name">First name:</label>
+          <input type="text" id="first-name" name="first-name"/>
+        <label htmlFor="last-name">Last name:</label>
+          <input type="text" id="last-name" name="last-name"/>
+        </form>
     </div>
   );
 }
